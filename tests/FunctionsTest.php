d1a93eecfb3387e911b90d904738e763a86bc881
KEINOS <github@keinos.com>
2018-05-25 11:03:32 +0900

    fix: Unit test fail

--- tests/FunctionsTest.php
+++ tests/FunctionsTest.php
@@ -9,6 +9,13 @@ class FunctionsTest extends TestCase
 
     public function setUp()
     {
+        // Warning も確実にエラーとして扱うようにする
+        set_error_handler(function ($errno, $errstr, $errfile, $errline) {
+            $msg  = 'Error #' . $errno . ': ';
+            $msg .= $errstr . " on line " . $errline . " in file " . $errfile;
+            throw new RuntimeException($msg);
+        });
+
         $this->array_sample1 = [
             'one' => true,
             'two' => false,
@@ -16,6 +23,7 @@ class FunctionsTest extends TestCase
         $this->string_sample1  = 'sample1';
         $this->string_default1 = 'default1';
     }
+
     public function testRequestApiIssue()
     {
         include_once('tests/test_request_api_issue.php.inc');
